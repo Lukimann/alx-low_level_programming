fde5a020248deb553a59ccbc09f2ac93a75b7c61
Lukimann <lukewanyama5@gmail.com>
2022-12-15 01:51:31 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,8 +11,14 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	1 = 0
-		printf("Infinite loop avoided! \\o/\n");
+	i = 0;
+	/**
+	* while (i < 10)
+	* {
+	* putchar(i);
+	* }
+	*/
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
