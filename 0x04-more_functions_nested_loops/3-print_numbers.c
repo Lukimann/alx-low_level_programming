d99201ecdaec5d68a12d733242076ad9561fb125
Lukimann <lukewanyama5@gmail.com>
2022-12-16 05:53:19 -0800

    Print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,16 @@
 
 /**
  * print_numbers - print numbers 0 to 9
- * @: unknown function
- * Return: void
+ * Return: Always 0 (success)
  */
 
 void print_numbers(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar('c');
+	_putchar('c');
 	}
 	_putchar('\n');
 }
