5e9a0f26f1ab1c3cac8459a2209377941e49663a
Lukimann <lukewanyama5@gmail.com>
2023-01-03 00:59:15 -0800

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - the copy set function
+ * @dest: destination
+ * @src: memory
+ * @n: an integer
+ * Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
