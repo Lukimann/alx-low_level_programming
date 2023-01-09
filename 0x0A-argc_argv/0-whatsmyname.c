a96489f59df957b05e206af38427998f7a0a5652
Lukimann <lukewanyama5@gmail.com>
2023-01-09 09:27:21 -0800

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function to print name
+ * @argc: argc parameter
+ * @argv: an array
+ * Return: Always 0 (success)
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *arg);
+	return (0);
+}
