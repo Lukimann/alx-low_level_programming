d8b5ed460a29b6312bfae95aad697fc5883e7282
Lukimann <lukewanyama5@gmail.com>
2023-01-10 23:55:16 -0800

    uno

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,13 +6,10 @@
  * @argv: an array
  * Return: Always 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i;
+	(void)argv;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
